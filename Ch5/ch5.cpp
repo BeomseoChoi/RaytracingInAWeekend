08bee9fe4bf754a5a5af3ae9071ca6f2bd693a6a
frijuly7 <frijuly7@gmail.com>
2023-12-19 20:37:49 +0900

    add ch5

--- Ch5/ch5.cpp
+++ Ch5/ch5.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include "../Utils/ray.h"
-#include "../Utils/vec3.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include <climits>
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0f * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-
-	float discriminant = b * b - 4 * a * c;
-
-	return (discriminant > 0);
-}
-
-vec3 color(const ray& r) {
-	if (hit_sphere(vec3(0, 0, -1), 0.5f, r)) {
-		return vec3(1, 0, 0);
+vec3 color(const ray& r, hitable* world) {
+	hit_record rec;
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
+		return 0.5f * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
@@ -42,6 +33,11 @@ int main() {
 	vec3 vertical(0.0f, 2.0f, 0.0f);
 	vec3 origin(0.0f, 0.0f, 0.0f);
 
+	hitable* list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5f);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable* world = new hitable_list(list, 2);
+
 	// PPM image coordinates. (LT, RT, LB, RB)
 	// (ny - 1, 0) ~ (ny - 1, nx - 1)
 	// (0, 0) ~ (0, 0)
@@ -51,7 +47,7 @@ int main() {
 			float v = float(j) / float(ny);
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r);
+			vec3 col = color(r, world);
 
 			int ir = int(255.99 * col.r());
 			int ig = int(255.99 * col.g());
